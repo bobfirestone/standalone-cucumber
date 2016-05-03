fab3d28c8a80b67b6a7ebf9a717511725919187e
Bob Firestone <bob@neonruby.com>
2016-05-03 12:13:44 -0600

    updating to latest gems

--- Gemfile
+++ Gemfile
@@ -1,8 +1,8 @@
 source "https://rubygems.org"
 
 group(:test) do
-  gem 'cucumber'
-  gem 'capybara'
-  gem 'rspec'
-  gem 'selenium-webdriver'
+  gem 'cucumber',           '~> 2.3'
+  gem 'capybara',           '~> 2.3'
+  gem 'rspec',              '~> 3.4'
+  gem 'selenium-webdriver', '~> 2.53'
 end
--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -3,11 +3,10 @@ Given /^I am on the home page$/ do
 end
 
 Given /^I have entered "([^"]*)" into the "([^"]*)" field$/ do |text, field|
-  fill_in field, :with => text
+  fill_in field, with: text
 end
 
 Then /^I should see "([^"]*)"$/ do |text|
-  page.should have_content(text)
+  # page.should have_content(text)
+  expect(page).to have_content(text)
 end
-
-
